0dadb0798754755965cb31ac685a5d964b42a74c
Steven Atkinson <mrstebouk@gmail.com>
2024-02-10 19:22:29 +0000

    Added HTML UP template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Header -->
+<header id="header">
+    <div class="content">
+        <h1><a href="/">Make It Dark</a></h1>
+        <p>Bringing back the childhood nostalgia!</p>
+        <ul class="actions">
+            <li><a href="/game" class="button primary icon solid fa-gamepad">Play</a></li>
+        </ul>
+    </div>
+    <div class="image phone"><div class="inner"><img src="/images/screen.jpg" alt="" /></div></div>
+</header>
+
